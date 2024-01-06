8b4bc11a192defb36d2933221827c2d29ba3df1c
Sakshi Wankhede <sakwan132000@gmail.com>
2024-01-06 17:20:38 +0530

    store commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +14,10 @@ const firebaseConfig = {
   storageBucket: "netflixgpt-6ac39.appspot.com",
   messagingSenderId: "950830533466",
   appId: "1:950830533466:web:a454330f562a4bd61fc163",
-  measurementId: "G-BJW1XTPWWY"
+  measurementId: "G-BJW1XTPWWY",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth();
