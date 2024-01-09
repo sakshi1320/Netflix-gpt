e721bab6bb8651d5575d4f85bb80a8e186149c4a
Sakshi Wankhede <sakwan132000@gmail.com>
2024-01-09 11:05:17 +0530

    new addPopularMovie slice

--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -7,10 +7,13 @@ const moviesSlice = createSlice({
     addNowPlayingMovies: (state, action) => {
       state.NowPlayingMovies = action.payload;
     },
+    addPopularMovies: (state, action) => {
+      state.PopularMovies = action.payload;
+    },
     addTrailerVideo: (state, action) => {
       state.trailerVideo = action.payload;
     },
   },
 });
-export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies } = moviesSlice.actions;
 export default moviesSlice.reducer;
