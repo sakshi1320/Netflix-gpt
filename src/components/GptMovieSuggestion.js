c74b6fc5aab69e3175cd4e2a1aae66499528064a
Sakshi Wankhede <sakwan132000@gmail.com>
2024-01-19 17:07:22 +0530

    UI FIXED

--- src/components/GptMovieSuggestion.js
+++ src/components/GptMovieSuggestion.js
@@ -5,7 +5,7 @@ const GptMovieSuggestion = () => {
   const { movieNames, movieResult } = useSelector((store) => store.gpt);
   if (!movieNames) return null;
   return (
-    <div className="p-4 m-4 bg-black text-white bg-oopacity-80">
+    <div className="p-4 m-4 bg-black text-white bg-opacity-80">
       <div>
         {movieNames.map((movieNames, index) => (
           <MovieList key={movieNames} title={movieNames} movies={movieResult[index]} />
